ee10710ed73b16f9562e02410cd1dde85e17ac86
Christopher H. Casebeer <christopher@chc.name>
2017-03-26 15:03:32 -0400

    Refactor and cleanup
    n.b. loop seems slower; need to adjust timings

--- VFO.ino
+++ VFO.ino
@@ -4,14 +4,9 @@
 
 #define DEBUG
 
-//#include <ClickEncoder.h>
-//#include <TimerOne.h>
-
 #define ENCODER_OPTIMIZE_INTERRUPTS
 #include <Encoder.h>
 
-#define ACCELERATION 1.2
-#define DECELERATION .99
 #define MAX_ACCELERATION 100000
 #define QUIESCENCE_MAX 2500
 
@@ -23,42 +18,53 @@
 #define RESET_PIN 8
 #define DDS_CLOCK_FREQ 125000000
 
-LiquidCrystal_PCF8574 lcd(0x27);  // set the LCD address to 0x27 for a 16 chars and 2 line display
-Encoder myEnc(2, 3);
+#ifdef DEBUG
+#define PRINT_DEBUG() {\
+  Serial.print("Accel: ");\
+  Serial.print(acceleration);\
+  Serial.print("\tEnc: ");\
+  Serial.print(encoderValue);\
+  Serial.print("\tLoops: ");\
+  Serial.print(timer);\
+  Serial.print("\t");\
+  Serial.print(timerSmooth);\
+  Serial.print("\n");\
+}
+#endif
+
+// set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_PCF8574 lcd(0x27);
+
+// Use hardware interrupt pins D2 and D3 on the Pro Mini
+Encoder FrequencyEncoder(2, 3);
 
 double acceleration = 1.0;
-uint32_t frequency;
-int8_t encoderDirection;
+uint32_t frequency = 440;
+uint32_t timer = QUIESCENCE_MAX;
+double timerSmooth = QUIESCENCE_MAX;
 
 void setup() {
   int error;
-  // put your setup code here, to run once:
+  
   Serial.begin(115200);
   Serial.println("LCD...");
-
-  frequency = 440;
-
-  //encoder = new ClickEncoder(2, 3);
-  //encoder->setAccelerationEnabled(true);
   
-  // put your main code here, to run repeatedly:
   while (! Serial);
 
-  Serial.println("Dose: check for LCD");
+  Serial.println("Checking for LCD...");
 
   // See http://playground.arduino.cc/Main/I2cScanner
   Wire.begin();
   Wire.beginTransmission(0x27);
   error = Wire.endTransmission();
-  Serial.print("Error: ");
-  Serial.print(error);
-
+  
   if (error == 0) {
-    Serial.println(": LCD found.");
-
+    Serial.println("LCD found.");
   } else {
+    Serial.print("Error ");
+    Serial.print(error);
     Serial.println(": LCD not found.");
-  } // if
+  }
 
   lcd.begin(16, 2); // initialize the lcd
   lcd.setBacklight(64);
@@ -80,85 +86,51 @@ void setFrequency(uint32_t f) {
   DDS.setfreq(f, 0);
 }
 
-uint32_t timer = QUIESCENCE_MAX;
-double timerSmooth = QUIESCENCE_MAX;
 void loop() {
   int32_t encoderValue;
-  timer++;
+
+  // increment timer; avoid overflows
+  timer = timer >= QUIESCENCE_MAX ? QUIESCENCE_MAX : timer + 1;
   
-  encoderValue = myEnc.read();
-  myEnc.write(0);
+  encoderValue = FrequencyEncoder.read();
+  FrequencyEncoder.write(0);
 
-  //if (encoderValue != 0 )
-  //Serial.println(encoderValue);
-
+  // Ignore inputs with absolute value > 1 
+  // We're reading the encoder fast enough that these are all noise
   if (encoderValue > 1 || encoderValue < -1) {
     encoderValue = 0;
   }
 
   // encoder debouncing; ignore inputs for 10% of the moving average inter-input time
-  if (timer < timerSmooth / 10 && timer < QUIESCENCE_MAX) {
-    return;
-  }
-  if ( encoderValue != 0) {
-    timerSmooth = .2 * timer + (1-.2) * timerSmooth;
+  if (timer >= timerSmooth / 10 || timer >= QUIESCENCE_MAX) {
+      
+    // base acceleration on the speed of turning, as determined by debounced
+    // inter-encoder-input timings
+    acceleration = double(100000.0) / pow(timerSmooth / 100., 7);
+    if (acceleration < 1) {
+      acceleration = 1;
+    } else if (acceleration > MAX_ACCELERATION) {
+      acceleration = MAX_ACCELERATION;
+    }
     
-    if (encoderValue != encoderDirection) {
-      // reset acceleration when changing direction
-      //acceleration *= .1;
-    }
-    frequency += encoderValue * int32_t(acceleration);
-    if (frequency > FREQUENCY_MAX) {
-      frequency = (FREQUENCY_MAX + encoderValue * int32_t(acceleration)) % FREQUENCY_MAX;
-    }
-
-    setFrequency(frequency);
-
-    // -1 or +1
-    encoderDirection = encoderValue;
+    if (encoderValue != 0) {
+      timerSmooth = .2 * timer + (1-.2) * timerSmooth;
 #ifdef DEBUG
-  
-    Serial.print("Accel: ");
-    Serial.print(acceleration);
-    Serial.print("\tEnc: ");
-    Serial.print(encoderValue);
-    Serial.print("\tLoops: ");
-    Serial.print(timer);
-    Serial.print("\t");
-    Serial.print(timerSmooth);
-    Serial.print("\n");
-                
-    /*
-    Serial.print(encoderValue);
-    Serial.print("\tnewValue (accelerated): ");
-    Serial.print(encoderValue * int32_t(acceleration));
-    Serial.print("\tfrequency: ");
-    Serial.print(frequency);
-    Serial.println("");
-    */
+      PRINT_DEBUG();
 #endif
-    timer = 0;
-
-
-  }
-  //acceleration *= encoderValue == 0 ? DECELERATION : ACCELERATION;
-  double ts = timerSmooth / 100., 
-    ts3 = ts * ts * ts;
-  acceleration = double(100000.0) / (ts3 * ts3 * ts);
-  if (acceleration > 1) {
-    //Serial.print("Accleration: ");
-    //Serial.print(acceleration);
-    //Serial.print("\n");
-  }
-  if (acceleration < 1) {
-    acceleration = 1;
-  } else if (acceleration > MAX_ACCELERATION) {
-    acceleration = MAX_ACCELERATION;
-  }
+      timer = 0;
   
+      frequency += encoderValue * int32_t(acceleration);
   
+      if (frequency > FREQUENCY_MAX) {
+        frequency = (FREQUENCY_MAX + encoderValue * int32_t(acceleration)) % FREQUENCY_MAX;
+      }
   
+      setFrequency(frequency);
+    }  
+  }
 }
+
 
 #define Hz(f) ((uint16_t)(f % 1000))
 #define kHz(f) ((uint16_t)((f % 1000000) / 1000))
